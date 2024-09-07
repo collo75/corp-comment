2ee0b499f37a1e29c2a2ea70f1e9529d8c22ddc3
Steve <steve@tucbox.com>
2024-09-07 08:55:01 +0100

    extracted feedbackItem to separate component, enforced types in props,used asyc await to fetch API dummy data, added loading and error states

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,21 +1,23 @@
 import { useState } from "react";
-import { MAX_CHARACTERS } from "../lib/constant";
+import { MAX_CHARACTERS } from "../lib/constants";
 
 export default function FeedbackForm() {
   const [text, setText] = useState("");
   const charCount = MAX_CHARACTERS - text.length;
 
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = event.target.value;
+    if (newText.length > MAX_CHARACTERS) {
+      return;
+    }
+    setText(newText);
+  };
+
   return (
     <form className="form">
       <textarea
         value={text}
-        onChange={(event) => {
-          const newText = event.target.value;
-          if (newText.length > MAX_CHARACTERS) {
-            return;
-          }
-          setText(newText);
-        }}
+        onChange={handleChange}
         id="feedback-textarea"
         placeholder="blabla"
         spellCheck={false}
--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,28 +1,46 @@
-import { TriangleUpIcon } from "@radix-ui/react-icons";
+import { useEffect, useState } from "react";
+import FeedbackItem from "./FeedbackItem";
+import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const fetchFeedbackItems = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(
+          "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+        );
+
+        if (!response.ok) {
+          throw new Error();
+        }
+
+        const data = await response.json();
+        setFeedbackItems(data.feedbacks);
+      } catch (error) {
+        setErrorMessage("Failed to load feedback items");
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchFeedbackItems();
+  }, []);
+
   return (
     <ol className="feedback-list">
-      <li className="feedback">
-        <button>
-          <TriangleUpIcon />
-          <span>593</span>
-        </button>
+      {isLoading && <Spinner />}
 
-        <div>
-          <p>B</p>
-        </div>
-
-        <div>
-          <p>GliuDigital</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
-            eligendi aliquid consectetur ullam aliquam corporis?
-          </p>
-        </div>
-
-        <p>4d</p>
-      </li>
+      {errorMessage && <ErrorMessage message={errorMessage} />}
+      {feedbackItems.map((feedbackItem) => (
+        <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
+      ))}
     </ol>
   );
 }
